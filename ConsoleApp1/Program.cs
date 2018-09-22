292ff68bd26f005817086ed0309b40487a067b07
Luis Nunez <lanr@hotmail.es>
2018-09-22 12:53:33 -0600

    last
    as

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LAB02;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Person");
+
+              //ILogger logger = new Persona();
+
+              //LAB v1
+              Persona p = new Persona();
+              p.Id = 1;
+              p.Name = "Luis";
+              p.LastName = "Nunez";
+
+
+            /* //LAB v2
+             * Persona p = new Persona(1, "Luis", "Nunez   ");
+             */
+
+            /* //LAB v3
+             * Persona p = new Persona
+             * {
+             * Id = 1,
+             * Name = "Luis",
+             * LastName = "Nunez"
+             * };
+             */
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            ILogger logger = p;
+
+            String personString = string.Format("ID: {0}, Name: {1} and Last Name: {2}.", p.Id, p.Name, p.LastName);
+
+            //Console.WriteLine(personString);
+            logger.Log(personString);
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LAB02/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB02
+{
+    public class Persona : ILogger
+    {
+
+        #region Variables
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        #endregion
+
+        #region Construtores
+        public Persona()
+        {}
+        
+        public Persona(int id, string name, string lastName)
+        {
+            Id = id;
+            Name = name;
+            LastName = lastName;
+        }
+        #endregion
+
+        #region Metodos
+        public void Log(string log)
+        {
+            Console.WriteLine("Using logger.");
+        }
+        #endregion
+
+    }
+}
